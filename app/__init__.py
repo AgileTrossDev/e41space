1e9309335b2e72cea255499e1a978430a5a92094
Agile Tross <scott.jackson.swe@gmail.com>
2018-10-09 00:20:19 -0600

    Chapter 5 - User Registration/login stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
+# Flask and it's extentions are declared here for the App.
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
 
 # Import here to avoid circular dependancies
 from app import routes, models
